6e6a9e9f1c14b39b02bc036380565aee4d2b2636
Fadojutimi Temitayo Olusegun <fadojutimitemitayo@gmail.com>
2017-07-28 13:01:44 +0100

    fix index.pug responsiveness on md and sm viewports

--- routes/me.js
+++ routes/me.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('dashboard/me', {  });
+  res.render('dashboard/me', { title:'My profile' });
 });
 
 module.exports = router;
