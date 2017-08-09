ddcab0b6b65cdfd9f4fd4d12dc5517e8a612fc42
Fadojutimi Temitayo Olusegun <fadojutimitemitayo@gmail.com>
2017-08-09 15:25:40 +0100

    update userAuthenticate.js

--- server/src/middlewares/userAuthenticate.js
+++ server/src/middlewares/userAuthenticate.js
@@ -1,7 +1,6 @@
 const router = (req, res, next) => {
   const group = req.decoded.group;
   if (group !== 'user') {
-    console.log(group);
     return res.status(403).send({
       status: 'Forbidden',
       message: 'Well, you need to be a user to go in here',
