ab49884427ef665f55f36afc091783f951786fa3
Fadojutimi Temitayo Olusegun <fadojutimitemitayo@gmail.com>
2017-08-09 15:14:16 +0100

    change stock management to stock manager

--- routes/book/stock/view.js
+++ routes/book/stock/view.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('book/stock/view', { title:'Stock Management' });
+  res.render('book/stock/view', { title:'Stock Manager' });
 });
 
 module.exports = router;
