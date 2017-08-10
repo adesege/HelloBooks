b9291fc5e77086cc576a82ee7c463dabcea6a6a9
Fadojutimi Temitayo Olusegun <fadojutimitemitayo@gmail.com>
2017-08-10 20:00:29 +0100

    update app.js

--- server/dist/app.js
+++ server/dist/app.js
@@ -28,7 +28,13 @@ var _routes = require('./routes');
 
 var _routes2 = _interopRequireDefault(_routes);
 
+var _dotenv = require('dotenv');
+
+var _dotenv2 = _interopRequireDefault(_dotenv);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_dotenv2.default.config();
 
 var app = (0, _express2.default)();
 var renderFile = _ejs2.default.renderFile;
