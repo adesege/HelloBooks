8f552613e40eacdb74d615e86915c53784a5e357
Fadojutimi Temitayo Olusegun <fadojutimitemitayo@gmail.com>
2017-08-15 15:09:26 +0100

    resolve merge conflicts

--- server/dist/migrations/20170803202624-update-book-migration.js
+++ server/dist/migrations/20170803202624-update-book-migration.js
@@ -1,6 +1,7 @@
+'use strict';
 
 module.exports = {
-  up(queryInterface, Sequelize) {
+  up: function up(queryInterface, Sequelize) {
     /* return queryInterface.addConstraint('User', ['id'], {
       type: 'FOREIGN KEY',
       references: { // Required field
@@ -13,8 +14,7 @@ module.exports = {
       type: Sequelize.String
     }); */
   },
-
-  down(queryInterface) {
+  down: function down(queryInterface) {
     queryInterface.removeColumn('User', 'id');
   }
 };
