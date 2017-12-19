4e083b38a08a0115e5b17e524af9e8f672aaab0a
Fadojutimi Temitayo Olusegun <Fadojutimitemitayo@gmail.com>
2017-12-19 06:38:29 +0100

    chore(e2e-test): setup and write test for end-to-end testing using nightwatch

---
+++ e2e-tests/signin.spec.js
@@ -0,0 +1,49 @@
+module.exports = {
+  'User should not be able to signin if no email address is provided': (browser) => {
+    browser
+      .url(browser.launchUrl)
+      .waitForElementVisible('body', 1000)
+      .setValue('#email', '')
+      .setValue('#password', '')
+      .click('#login')
+      .pause(1000)
+      .assert.containsText('.form-text', 'This field is required')
+      .end();
+  },
+  'User should not be able to signin when the email address does not exist': (browser) => {
+    browser
+      .url(browser.launchUrl)
+      .waitForElementVisible('body', 1000)
+      .setValue('#email', 'helloworld@us.com')
+      .setValue('#password', 'hello world')
+      .click('#login')
+      .pause(1000)
+      .assert.containsText('.alert-danger', 'You provided a wrong email address and password')
+      .end();
+  },
+  'User should be able to signin with a valid credentials': (browser) => {
+    browser
+      .url(browser.launchUrl)
+      .waitForElementVisible('body', 1000)
+      .setValue('#email', 'helloworld@us.com')
+      .setValue('#password', 'helloworld@us.com')
+      .click('#login')
+      .pause(1000)
+      .waitForElementPresent('#contentArea', 1000)
+      .assert.urlEquals(`${browser.launchUrl}/dashboard`)
+      .end();
+  },
+  'User should be able to logout after successfully sigining in ': (browser) => {
+    browser
+      .url(browser.launchUrl)
+      .waitForElementVisible('body', 1000)
+      .setValue('#email', 'helloworld@us.com')
+      .setValue('#password', 'helloworld@us.com')
+      .click('#login')
+      .pause(1000)
+      .waitForElementPresent('#contentArea', 1000)
+      .assert.urlEquals(`${browser.launchUrl}/dashboard`)
+      .click('a.nav-link.text-danger')
+      .end();
+  }
+};
